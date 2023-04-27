064bdf20f6b77bf8af2e873b6093a6a2ae8bf2b5
amdazm05 <amdali1998@yahoo.com>
2023-04-27 07:31:15 +0500

    feat: work on streamer class

---
+++ include/stream/serialstreamer.hpp
@@ -0,0 +1,28 @@
+#ifndef _SERIALSTREAMER
+#define _SERIALSTREAMER
+
+#include <array>
+#include <string>
+#include <memory>
+#include <vector>
+
+class SerialStreamer
+{
+    public:
+        SerialStreamer();
+        ~SerialStreamer();
+        void enableDelimeterSearchMode(bool condition);
+        void setListOfDelimitersToFind(std::vector<uint32_t> && listofDelimiters);
+        void init(std::string && string);
+        bool isAValidPacketAvailable();
+        template<typename T>
+        std::size_t ReadAvailableData( T * dataBufferToReadTo);
+        void writeData(std::shared_ptr<char> dataToWrite, std::size_t bytesToWrite);
+    private:
+        std::vector<uint32_t> listofDelimiters;
+        std::array<char,1<<16> recieveBuffer;
+        std::unique_ptr<char> currentBufferLocation;
+}
+
+
+#endif
