2dbc696ce2afb21dbcfadb05659afac2130be4c2
amdazm05 <amdali1998@yahoo.com>
2023-04-29 18:53:55 +0500

    WIP: blue print for windows based streamer with CRTP design

--- include/stream/serialstreamer.hpp
+++ include/stream/serialstreamer.hpp
@@ -6,23 +6,42 @@
 #include <memory>
 #include <vector>
 
-class SerialStreamer
-{
-    public:
-        SerialStreamer();
-        ~SerialStreamer();
-        void enableDelimeterSearchMode(bool condition);
-        void setListOfDelimitersToFind(std::vector<uint32_t> && listofDelimiters);
-        void init(std::string && string);
-        bool isAValidPacketAvailable();
-        template<typename T>
-        std::size_t ReadAvailableData( T * dataBufferToReadTo);
-        void writeData(std::shared_ptr<char> dataToWrite, std::size_t bytesToWrite);
-    private:
-        std::vector<uint32_t> listofDelimiters;
-        std::array<char,1<<16> recieveBuffer;
-        std::unique_ptr<char> currentBufferLocation;
-}
+    template<class D>
+    class SerialStreamer
+    {
+        public:
+            SerialStreamer();
+            ~SerialStreamer();
+            void enableDelimeterSearchMode(bool condition);
+            void setListOfDelimitersToFind(std::vector<uint32_t> && listofDelimiters);
+            void init(std::string && string);
+            bool isAValidPacketAvailable();
+            template<typename T>
+            std::size_t ReadAvailableData( T * dataBufferToReadTo);
+            void writeData(std::shared_ptr<char> dataToWrite, std::size_t bytesToWrite);
+        private:
+            std::vector<uint32_t> listofDelimiters;
+            std::array<char,1<<16> recieveBuffer;
+            std::unique_ptr<char> currentBufferLocation;
+    };
 
+    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+    
+    class WindowsSerialStreamer : SerialStreamer<WindowsSerialStreamer>
+    {
+        public:
+            WindowsSerialStreamer();
+            ~WindowsSerialStreamer();
+            void enableDelimeterSearchMode(bool condition);
+            void setListOfDelimitersToFind(std::vector<uint32_t> && listofDelimiters);
+            void init(std::string && string);
+            bool isAValidPacketAvailable();
+            template<typename T>
+            std::size_t ReadAvailableData( T * dataBufferToReadTo);
+            void writeData(std::shared_ptr<char> dataToWrite, std::size_t bytesToWrite);
+    };
+
+
+    #endif
 
 #endif
