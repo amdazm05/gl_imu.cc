a1dac5174dd001caf7664e901423ec78ac521f92
amdazm05 <amdali1998@yahoo.com>
2023-05-18 23:05:49 +0500

    submodule: freetype

--- include/graphics/text_component.hpp
+++ include/graphics/text_component.hpp
@@ -3,19 +3,22 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <ft2build.h>
+#include FT_FREETYPE_H  
 #include <memory>
 #include <unordered_map>
 #include <string>
+
 #include "util/functionutils.tpp"
 
-class TextComponent : std::enable_shared_from_this()
+class TextComponent : std::enable_shared_from_this<TextComponent>
 {
     public:
-        TextComponent();
+        TextComponent(std::shared_ptr<GLFWwindow> windowInstance);
+        TextComponent() = delete;
         ~TextComponent();
         void renderText(std::string && inputText,std::pair<double,double> renderPosition,std::tuple<uint8_t,uint8_t,uint8_t> color);
         std::shared_ptr<TextComponent> getInstance();
-
 
     private:
         function_utils::function_ptr<void(*)(GLFWwindow* window, unsigned int codepoint)> _characterCallback;
