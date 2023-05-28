765221041ef2722b6e3dab568fc286faa118cbf2
amdazm05 <amdali1998@yahoo.com>
2023-05-29 00:41:37 +0500

    feat: added functionality to cache display lists .

--- src/graphics/text_component.cc
+++ src/graphics/text_component.cc
@@ -42,17 +42,24 @@ void TextComponent::init(std::string && fontFile, uint32_t textheight)
     // glGenTextures generates the unique names provided by GL taht are stored in textures
     if(_textures != nullptr)
         glGenTextures(CHAR_MAX_COUNT,_textures.get());
+
+    for(uint8_t i=0;i<CHAR_MAX_COUNT;i++)
+        make_display_lists((char)i,_face);
+
+    FT_Done_Face(_face);
+	//Ditto for the library.
+	FT_Done_FreeType(_library);
 }
 
 void TextComponent::make_display_lists(char character, FT_Face faceCpy)
 {
-    _error = FT_Load_Glyph(_face,FT_Get_Char_Index(_face,character),FT_LOAD_DEFAULT); 
+    _error = FT_Load_Glyph(faceCpy,FT_Get_Char_Index(faceCpy,character),FT_LOAD_DEFAULT); 
     if(_error)
     {
         throw std::runtime_error("Text Component: Failed to load text component");
     }
 
-    _error = FT_Get_Glyph(_face->glyph,&_glyph);
+    _error = FT_Get_Glyph(faceCpy->glyph,&_glyph);
     if(_error)
     {
         throw std::runtime_error("Text Component: Failed to create bitmap from glyph");
@@ -89,6 +96,35 @@ void TextComponent::make_display_lists(char character, FT_Face faceCpy)
                     (i >= bitmap.width || j >= bitmap.rows) ? 0 : bitmap.buffer[i + bitmap.width * j];
             }
         }
+
+        glBindTexture( GL_TEXTURE_2D, _textures.get()[character]);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height,
+            0, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, _expanded_bitmapdata.get());
     }
+
+    glNewList(_listbase+character,GL_COMPILE);
+
+	glBindTexture(GL_TEXTURE_2D,_textures.get()[character]);
+	glTranslatef(_bitmapGlyph->left,0,0);
+
+	glPushMatrix();
+	glTranslatef(0,_bitmapGlyph->top-bitmap.rows,0);
+
+	float	x=(float)bitmap.width / (float)width,
+			y=(float)bitmap.rows / (float)height;
+
+
+	glBegin(GL_QUADS);
+	glTexCoord2d(0,0); glVertex2f(0,bitmap.rows);
+	glTexCoord2d(0,y); glVertex2f(0,0);
+	glTexCoord2d(x,y); glVertex2f(bitmap.width,0);
+	glTexCoord2d(x,0); glVertex2f(bitmap.width,bitmap.rows);
+	glEnd();
+	glPopMatrix();
+	glTranslatef(faceCpy->glyph->advance.x >> 6 ,0,0);
+
+	glEndList();
 
 }
