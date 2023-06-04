a0b95dd85ea58074c7218a5bacf763eca7bfb1ac
amdazm05 <amdali1998@yahoo.com>
2023-06-04 20:29:30 +0500

    feat: glm , refactoring text component

--- include/graphics/text_component.hpp
+++ include/graphics/text_component.hpp
@@ -8,14 +8,28 @@
 #include <freetype/ftglyph.h>
 #include <freetype/ftoutln.h>
 #include <freetype/fttrigon.h>
-
 #include FT_FREETYPE_H  
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <memory>
 #include <iostream>
 #include <unordered_map>
 #include <string>
 
 #include "util/functionutils.tpp"
+
+#define GL_CLAMP_TO_EDGE 0x812F
+/// Holds all state information relevant to a character as loaded using FreeType
+struct Character 
+{
+    unsigned int TextureID; // ID handle of the glyph texture
+    glm::ivec2   Size;      // Size of glyph
+    glm::ivec2   Bearing;   // Offset from baseline to left/top of glyph
+    unsigned int Advance;   // Horizontal offset to advance to next glyph
+};
 
 class TextComponent : std::enable_shared_from_this<TextComponent>
 {
@@ -29,7 +43,7 @@ class TextComponent : std::enable_shared_from_this<TextComponent>
     private:
         /// @brief 
         // generates a display list in GL for a single character
-        void make_display_lists(char character, FT_Face faceCpy);
+        void make_display_lists(char character);
 
         function_utils::function_ptr<void(*)(GLFWwindow* window, unsigned int codepoint)> _characterCallback;
         /// @brief each library is independant from oneanother, root of fonts, faces, size
@@ -52,13 +66,14 @@ class TextComponent : std::enable_shared_from_this<TextComponent>
         FT_Bitmap _bitmap;
         //Allocates memory for each bitmap when characters are initialised
         std::shared_ptr<GLubyte> _expanded_bitmapdata; 
-
-        
         //Error handling // Will be used for exceptions
         FT_Error _error;
+
+        std::unordered_map<char , Character> _charactermap;
         
         // Max character count
         static constexpr uint8_t CHAR_MAX_COUNT = 128;
+        
     public:
         friend int neares_pow_2(int a);
 
--- src/graphics/text_component.cc
+++ src/graphics/text_component.cc
@@ -39,152 +39,53 @@ void TextComponent::init(std::string && fontFile, uint32_t textheight)
     {
         throw std::runtime_error("Text Component : Failed to create face");
     }
-    _textheight = textheight;
-    FT_Set_Char_Size(_face, _textheight<<6, _textheight<<6, 96,96);
+    else
+    {
+        FT_Set_Pixel_Sizes(_face,0,48);
+        glPixelStorei(GL_UNPACK_ALIGNMENT,1);
+        for(char c =0 ;c< CHAR_MAX_COUNT;c++)
+            make_display_lists(c);
 
-    // List base is the count of contiguous "empty" lists allocated by GL
-    _listbase = glGenLists(CHAR_MAX_COUNT);
-
-    // glGenTextures generates the unique names provided by GL taht are stored in textures
-    if(_textures != nullptr)
-        glGenTextures(CHAR_MAX_COUNT,_textures.get());
-
-    for(uint8_t i=0;i<CHAR_MAX_COUNT;i++)
-        make_display_lists((char)i,_face);
-
-    FT_Done_Face(_face);
-	//Ditto for the library.
-	FT_Done_FreeType(_library);
+    }
 }
 
-void TextComponent::make_display_lists(char character, FT_Face faceCpy)
+void TextComponent::make_display_lists(char c)
 {
-    _error = FT_Load_Glyph(faceCpy,FT_Get_Char_Index(faceCpy,character),FT_LOAD_DEFAULT); 
+    _error = FT_Load_Char(_face,c,FT_LOAD_RENDER); 
     if(_error)
     {
         throw std::runtime_error("Text Component: Failed to load text component");
     }
 
-    _error = FT_Get_Glyph(faceCpy->glyph,&_glyph);
-    if(_error)
-    {
-        throw std::runtime_error("Text Component: Failed to create bitmap from glyph");
-    }
-    
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(
+        GL_TEXTURE_2D,
+        0,
+        GL_RED,
+        _face->glyph->bitmap.width,
+        _face->glyph->bitmap.rows,
+        0,
+        GL_RED,
+        GL_UNSIGNED_BYTE,
+        _face->glyph->bitmap.buffer
+    );
 
-    _error = FT_Glyph_To_Bitmap(&_glyph,ft_render_mode_normal,0,true);
-    if(_error)
-    {
-        throw std::runtime_error("Text Component: Failed to create bitmap from glyph");
-    }
-
-    _bitmapGlyph = (FT_BitmapGlyph)_glyph;
-    FT_Bitmap& bitmap = _bitmapGlyph->bitmap;
-
-    int width   = neares_pow_2(bitmap.width);
-    int height  = neares_pow_2(bitmap.rows);
-
-    {
-        //Scoped
-        _expanded_bitmapdata = std::shared_ptr<GLubyte>(new GLubyte[2* width * height],
-            [](GLubyte * data)
-            {
-                delete [] data;
-            }
-        );
-
-        for (int j = 0; j < height; j++)
-        {
-            for (int i = 0; i < width; i++)
-            {
-                _expanded_bitmapdata.get()[2 * (i + j * width)] 
-                = _expanded_bitmapdata.get()[2 * (i + j * width) + 1] =
-                    (i >= bitmap.width || j >= bitmap.rows) ? 0 : bitmap.buffer[i + bitmap.width * j];
-            }
-        }
-
-        glBindTexture( GL_TEXTURE_2D, _textures.get()[character]);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height,
-            0, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, _expanded_bitmapdata.get());
-    }
-
-    glNewList(_listbase+character,GL_COMPILE);
-
-	glBindTexture(GL_TEXTURE_2D,_textures.get()[character]);
-	glTranslatef(_bitmapGlyph->left,0,0);
-
-	glPushMatrix();
-	glTranslatef(0,_bitmapGlyph->top-bitmap.rows,0);
-
-	float	x=(float)bitmap.width / (float)width,
-			y=(float)bitmap.rows / (float)height;
-
-
-	glBegin(GL_QUADS);
-	glTexCoord2d(0,0); glVertex2f(0,bitmap.rows);
-	glTexCoord2d(0,y); glVertex2f(0,0);
-	glTexCoord2d(x,y); glVertex2f(bitmap.width,0);
-	glTexCoord2d(x,0); glVertex2f(bitmap.width,bitmap.rows);
-	glEnd();
-	glPopMatrix();
-	glTranslatef(faceCpy->glyph->advance.x >> 6 ,0,0);
-
-	glEndList();
-
-}
-
-void pushScreenCoordinateMatrix() {
-	glPushAttrib(GL_TRANSFORM_BIT);
-	GLint	viewport[4];
-	glGetIntegerv(GL_VIEWPORT, viewport);
-	glMatrixMode(GL_PROJECTION);
-	glPushMatrix();
-	glLoadIdentity();
-	gluOrtho2D(viewport[0],viewport[2],viewport[1],viewport[3]);
-	glPopAttrib();
-}
-void popProjectionMatrix() {
-	glPushAttrib(GL_TRANSFORM_BIT);
-	glMatrixMode(GL_PROJECTION);
-	glPopMatrix();
-	glPopAttrib();
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    Character character = {
+                texture,
+                glm::ivec2(_face->glyph->bitmap.width, _face->glyph->bitmap.rows),
+                glm::ivec2(_face->glyph->bitmap_left, _face->glyph->bitmap_top),
+                static_cast<unsigned int>(_face->glyph->advance.x)
+            };
+    _charactermap.insert(std::pair<char, Character>(c, std::move(character)));
 }
 
 void TextComponent::printtxt(std::string && text ,std::pair<float,float> position)  
 {
-	float heightAdjusted =  _height/.63f;
-    glColor3ub(0,0,0xff);
-    glPushMatrix();
-	glLoadIdentity();
-	glScalef(1,.8+.3,1);
-	glTranslatef(-120,0,0);
-	pushScreenCoordinateMatrix();						
-	GLuint font= _listbase;
-
-	glPushAttrib(GL_LIST_BIT | GL_CURRENT_BIT  | GL_ENABLE_BIT | GL_TRANSFORM_BIT);	
-	glMatrixMode(GL_MODELVIEW);
-	glDisable(GL_LIGHTING);
-	glEnable(GL_TEXTURE_2D);
-	glDisable(GL_DEPTH_TEST);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);	
-
-	glListBase(font);
-
-	float modelview_matrix[16];	
-	glGetFloatv(GL_MODELVIEW_MATRIX, modelview_matrix);	
-
-    glPushMatrix();
-    glLoadIdentity();
-    glTranslatef(position.first,position.second,0);
-    glMultMatrixf(modelview_matrix);
-    glCallLists(text.length(), GL_UNSIGNED_BYTE, text.c_str());
-    glPopMatrix();
-    glPopAttrib();
-
-	popProjectionMatrix();
-    glPopMatrix();
 }
 
