59b7e184fb8fd71879771cb2c9570341855dfc5d
amdazm05 <amdali1998@yahoo.com>
2023-06-27 16:34:37 +0500

    WIP: updated error handling class design

--- include/graphics/error_logger.hpp
+++ include/graphics/error_logger.hpp
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <source_location>
 #include <memory>
+#include <iomanip>
 #include <fstream>
 
 class GraphicsErrorHandler : std::enable_shared_from_this<GraphicsErrorHandler> 
@@ -13,10 +14,12 @@ class GraphicsErrorHandler : std::enable_shared_from_this<GraphicsErrorHandler>
     public:
         GraphicsErrorHandler(std::string && errorFilename, std::shared_ptr<GladGLContext> windowContext);
         ~GraphicsErrorHandler();
-        void LogErrors(const std::source_location location  = std::source_location::current());
+        void LogErrors(const std::source_location location = std::source_location::current());
     private:
         GLenum _err; 
-        std::fstream _errorFilep;
-}
+        std::unique_ptr<std::fstream,void(*)(std::fstream *)> _errorFilep;
+        std::shared_ptr<GladGLContext> _windowContext;
+        std::chrono::system_clock::time_point _now;
+};
 
 #endif //_ERROR_LOGGER
