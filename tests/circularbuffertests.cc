54a8ca933419d3f9190386435b39ce524f7fd550
amdazm05 <amdali1998@yahoo.com>
2023-07-16 00:47:09 +0500

    feat: passing test , ready for another merge

--- tests/circularbuffertests.cc
+++ tests/circularbuffertests.cc
@@ -1,18 +1,25 @@
-#include <iostream>
+#include "gtest/gtest.h"
 #include "util/containers/circularbuffer.tpp"
 
-struct Data
+namespace
 {
-    int a;
-    int b;
-};
-
-int main()
-{
+    struct Data
+    {
+        int a;
+        int b;
+    };
     CircularBuffer<Data,16> _data;
-    Data data {1,2};
-    _data.enqueue((data));
-    Data data2 {1,2};
-    _data.enqueue((data2));
-    return 0;
-}
+    TEST(EnqueueDequeueTest, CoordinateConversionTests) 
+    {
+        Data data {1,2};
+        _data.enqueue((data));
+        Data data2 {3,4};
+        _data.enqueue((data2));
+        Data data4 = _data.dequeue();
+        Data data5 = _data.dequeue();
+        EXPECT_EQ(true,  data.a == data4.a);
+        EXPECT_EQ(true,  data.b == data4.b);
+        EXPECT_EQ(true,  data2.a == data5.a);
+        EXPECT_EQ(true,  data2.b == data5.b);
+    }
+};
